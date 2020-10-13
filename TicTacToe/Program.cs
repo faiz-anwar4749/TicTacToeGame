6dcf0e2e01b16c42b51578ae477d0620429496e5
Faiz <faiz.anwar4749@gmail.com>
2020-10-13 09:47:10 +0530

    [Faiz] Adding : Program to create a board

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game.");
+            char[] board = TicTacToeGame.MakeBoard();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public static char[] MakeBoard()
+        {
+            char[] board = new char[10];
+            for(int i=0; i<board.Length;i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
